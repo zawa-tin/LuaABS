6c22e5922d53156e4b151da0d597230186f0f35c
zawa-tin <contactaizawa@icloud.com>
2023-05-15 05:46:44 +0900

    add F.lua

---
+++ F.lua
@@ -0,0 +1,16 @@
+A, B, C, X = io.read("*n", "*n", "*n", "*n");
+
+local function f(a, b, c)
+    return a * 500 + b * 100 + c * 50;
+end
+
+ans = 0;
+for a = 0, A do
+    for b = 0, B do
+        for c = 0, C do
+            if f(a, b, c) == X then ans = ans + 1 end
+        end
+    end
+end
+
+print(ans);
