e60fa139da6ac101fea3a238ba062b421d7a0e19
zawa-tin <contactaizawa@icloud.com>
2023-05-15 07:28:12 +0900

    add H.lua

---
+++ H.lua
@@ -0,0 +1,17 @@
+N = io.read("*n");
+A = {};
+for i = 1, N do
+    A[i] = io.read("*n");
+end
+
+table.sort(A);
+
+ans = 0;
+value = 0;
+for i = 1, N do
+    if value < A[i] then
+        ans, value = ans + 1, A[i]
+    end
+end
+
+print(ans);
