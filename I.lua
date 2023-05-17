e7c868f6295faef1d4746448eb228a9c1604e77c
zawa-tin <contactaizawa@icloud.com>
2023-05-17 18:33:59 +0900

    add I.lua

---
+++ I.lua
@@ -0,0 +1,25 @@
+N, Y = io.read("*n", "*n");
+
+function check(i, j, k)
+    local value = 10000 * i + 5000 * j + 1000 * k;
+    if value == Y then
+        return true;
+    else
+        return false;
+    end
+end
+
+function solve()
+    for i = 0, N do
+       for j = 0, N - i do
+           local k = N - i - j;
+           if check(i, j, k) then
+               return { i, j, k };
+           end
+       end
+    end
+    return { -1, -1, -1 };
+end
+
+ans = solve();
+print(table.concat(ans, " "));
